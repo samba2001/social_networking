82e1d4142ea0e4c50c03e86595d8f605b7d077ba
samba2001 <reddy200101@gmail.com>
2024-06-21 22:54:24 +0530

    added few changes model

--- user/authentication.py
+++ user/authentication.py
@@ -4,7 +4,7 @@ from .models import User
 
 class UserAuthentication(ModelBackend):
     def authenticate(self, request, username=None, password=None, **kwargs):
-        user = User.objects.get(email=request.data.get('email'))
+        user = User.objects.get(email=request.data.get('email').lower())
         if user.password == request.data.get('password'):
             return user
         else:
