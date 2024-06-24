0d3415e82eefa5c53055f3b46875047486484e88
samba2001 <reddy200101@gmail.com>
2024-06-24 12:45:56 +0530

    setings

--- social_networking/settings.py
+++ social_networking/settings.py
@@ -79,9 +79,13 @@ WSGI_APPLICATION = 'social_networking.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'social_networking',
+        'USER': 'root',
+        'PASSWORD': 'medplus123',
+        'HOST': 'localhost',
+        'PORT': 3306
+    },
 }
 
 # Password validation
